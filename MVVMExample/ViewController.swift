a0f806e089834cba333d198e10e33e450b8b1301
jackie <ss611611@gmail.com>
2024-10-15 14:33:50 +0800

    Initial commit

---
+++ MVVMExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMExample
+//
+//  Created by Jackie Lu on 2024/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
